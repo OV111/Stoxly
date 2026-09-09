8ccb0aa553d0792860f1aadf1ab7f110c141d43e
Vahe <ohanyanv703@gmail.com>
2026-09-09 16:22:01 +0400

    Toaster from sooner for search

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "sonner";
 const montserrat = Montserrat({
   variable: "--font-montserrat",
   subsets: ["latin"],
@@ -24,6 +24,7 @@ export default function RootLayout({
         className={`${montserrat.variable} font-[family-name:var(--font-montserrat)] antialiased`}
       >
         {children}
+        <Toaster richColors position="bottom-right" />
       </body>
     </html>
   );
